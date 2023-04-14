650430b35b352ca56d975fe5a5155d827e64acd6
axengine <axengine@gmail.com>
2023-04-14 15:10:46 +0800

    update check.sh

--- check.sh
+++ check.sh
@@ -1,11 +1,14 @@
- for dir in `ls .`
- do
-   if [ -d $dir ]
-   then
-     echo $dir
-     cd $dir
-     go build
-     cd ..
-   fi
-done
+#!/bin/bash
 
+# 查找所有.go文件
+for file in $(find . -name "*.go"); do
+  # 获取文件所在目录的路径
+  dir=$(dirname $file)
+  # 切换到该目录
+  echo $dir
+  cd $dir
+  # 执行 go build 命令
+  go build
+  # 返回上一级目录
+  cd -
+done
