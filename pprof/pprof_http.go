bc8ca8c7f90b4dea98c52d772f4dc8c06b6fab77
Lee0x273 <brucelee@yala.org>
2025-02-21 14:03:39 +0800

    update pprof

--- pprof/pprof_http.go
+++ pprof/pprof_http.go
@@ -1,25 +1,28 @@
 package pprof
 
 import (
+	"context"
 	"github.com/pkg/errors"
+	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"sync"
 	"time"
 )
 
-func MustStart(addr string) {
+func MustStart(listen string) {
 	go func() {
-		if err := http.ListenAndServe(addr, nil); err != nil {
-			panic(errors.Wrap(err, "pprof bind "+addr))
+		if err := http.ListenAndServe(listen, nil); err != nil {
+			panic(errors.Wrap(err, "pprof bind "+listen))
 		}
 	}()
 }
 
-func Start(addr string) error {
+func Start(listen string) error {
 	var ch chan error
 	go func() {
-		if err := http.ListenAndServe(addr, nil); err != nil {
-			ch <- errors.Wrap(err, "pprof bind "+addr)
+		if err := http.ListenAndServe(listen, nil); err != nil {
+			ch <- errors.Wrap(err, "pprof bind "+listen)
 			return
 		}
 		ch <- nil
@@ -31,4 +34,30 @@ func Start(addr string) error {
 	case <-time.After(time.Second):
 		return nil
 	}
+}
+
+// StartAsync run pprof http server async
+// usage:go Start(ctx,wg,listen)
+func StartAsync(ctx context.Context, wg *sync.WaitGroup, listen string) {
+	defer wg.Done()
+	server := &http.Server{
+		Addr:    listen,
+		Handler: http.DefaultServeMux,
+	}
+	wg.Add(1)
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println("start pprof server failed", err)
+		}
+		wg.Done()
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer shutdownCancel()
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Println("shutdown pprof server failed", err)
+	}
+	log.Println("pprof server stopped")
 }
--- pprof/pprof_http_test.go
+++ pprof/pprof_http_test.go
@@ -1,15 +1,38 @@
 package pprof
 
-import "testing"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"testing"
+)
 
 func TestMustStart(t *testing.T) {
-	MustStart(":8080")
+	MustStart(":6060")
 	select {}
 }
 
 func TestStart(t *testing.T) {
-	if err := Start(":8080"); err != nil {
+	if err := Start(":6060"); err != nil {
 		t.Fatal(err)
 	}
 	select {}
+}
+
+func TestStartAsync(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go StartAsync(ctx, &wg, ":6060")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	cancel()
+	wg.Wait()
 }
